ab6d31ad6b8c64d144f63e515b058e4f3c392d7b
Shelter <pradosh.balaji@gmail.com>
2022-03-05 20:52:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,44 @@
 const message=document.querySelector('.message');
 const checkButton=document.querySelector('.check');
 const randomNumber=Math.floor(Math.random()*20);
+const again=document.querySelector('.again');
 let score=20;
 
 checkButton.addEventListener('click',function(){
     const guess=document.querySelector('.guess').value;
-    const updateScore=document.querySelector('.score').value;
+    
     if(guess==randomNumber){
         message.textContent='You are right!';
+        document.querySelector('.highscore').textContent=score;
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+        document.querySelector('.number').textContent=randomNumber;
     }
-    else if(guess<=randomNumber){
+
+    else if(guess<randomNumber){
         message.textContent='Too low!';
         score=score-1;
         document.querySelector('.score').textContent=score;
     }
-    else{
+    
+    else if(guess>randomNumber){
+        if(score>0){
         message.textContent='Too high!';
         score=score-1;
         document.querySelector('.score').textContent=score;
+        
+        }
+        else {
+            message.textContent='You lose!';
+        }   
+    }
+    if(score==0){
+        again.textContent='Play again?';
+        again.addEventListener('click',function(){
+            location.reload();
+        });
     }
 });
+        
 
     
